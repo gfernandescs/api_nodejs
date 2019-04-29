67e8735389917e52de6cc81c6c76613532c3e0c9
Gabriel Fernandes <gfernandescs@gmail.com>
2019-04-29 11:06:54 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app     = express();
+const port    = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -7,4 +8,6 @@ app.use(express.urlencoded({ extended: false }));
 require('./app/controllers/authController')(app);
 require('./app/controllers/userController')(app);
 
-app.listen(3000);
+app.listen(port, function() {
+    console.log('Server started on port: ' + port);
+});
